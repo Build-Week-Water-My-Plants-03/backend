8d5ac5ac4cc2b9beae799edbe61f4dfa1f62e8c5
David <davidellis1020@gmail.com>
2021-08-23 23:09:43 -0700

    created seeds

--- api/data/seeds/02-plants.js.js
+++ api/data/seeds/02-plants.js.js
@@ -1,13 +1,27 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('plants').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('plants').insert([
+        {
+          nickname: "Beth",
+          species: "Daisies",
+          h2o_frequency: "1 inch per week",
+          image: "https://www.gardeningknowhow.com/wp-content/uploads/2020/06/daisies.jpg",
+        },
+        {
+          nickname: "Birdie",
+          species: "Birds of Paradise",
+          h2o_frequency: "Every 2 weeks",
+          image: "https://www.gardeningknowhow.com/wp-content/uploads/2020/11/bird-of-paradise-flower.jpg",
+        },
+        {
+          nickname: "Bamboo",
+          species: "Bamboo",
+          h2o_frequency: "Twice a week",
+          image: "https://www.gardeningknowhow.com/wp-content/uploads/2009/07/control-bamboo.jpg",
+        }
       ]);
     });
 };
