1248e830bce314eaeae46b8d9bac0af7781fac8e
David <davidellis1020@gmail.com>
2021-08-25 13:23:27 -0700

    wip

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const server = require("./api/server");
 
 const port = process.env.PORT || 5000;
 
-server.use(express.static(path.join(__dirname, "client/dist")));
+// server.use(express.static(path.join(__dirname, "client/dist")));
 
-server.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/dist", "index.html"));
-});
+// server.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client/dist", "index.html"));
+// });
 
 server.listen(port, () => {
     console.log("listening on " + port);
