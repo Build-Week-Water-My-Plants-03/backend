b244d76ac32dc8fa6703fc9acc8eeef775bd03a9
David <davidellis1020@gmail.com>
2021-08-26 14:20:48 -0700

    No clue

--- api/server.js
+++ api/server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
+
 const db = require('./data/db-config')
-const plantRouter = require("./api/plants/plants-router");
-const authRouter = require("./api/auth/auth-router");
-const userRouter = require("./api/users/users-router");
+// const plantRouter = require("./api/plants/plants-router");
+// const authRouter = require("./api/auth/auth-router");
+// const userRouter = require("./api/users/users-router");
 
 function getAllUsers() { return db('users') }
 function getAllPlants() { return db('plants') }
@@ -43,8 +44,8 @@ server.post('/api/plants', async (req, res) => {
   res.status(201).json(await insertPlants(req.body))
 })
 
-server.use("/api/plants", plantRouter);
-server.use("/api/auth", authRouter);
-server.use("/api/users", userRouter);
+// server.use("/api/plants", plantRouter);
+// server.use("/api/auth", authRouter);
+// server.use("/api/users", userRouter);
 
 module.exports = server
